b166bd3c2922a17ce42c4189483d99268cd9a22d
Jordi Deu-Pons <jordi@jordeu.net>
2012-10-02 09:18:50 +0200

    Update widget archtype

--- onexus-archetypes/widget/src/main/resources/archetype-resources/src/main/java/HelloWidgetCreator.java
+++ onexus-archetypes/widget/src/main/resources/archetype-resources/src/main/java/HelloWidgetCreator.java
@@ -18,7 +18,6 @@
 package ${package};
 
 import org.apache.wicket.model.IModel;
-import org.onexus.ui.IResourceRegister;
 import org.onexus.ui.website.WebsiteConfig;
 import org.onexus.ui.website.widgets.AbstractWidgetCreator;
 import org.onexus.ui.website.widgets.Widget;
@@ -34,13 +33,4 @@ public class HelloWidgetCreator extends AbstractWidgetCreator<HelloWidgetConfig,
         return new HelloWidget(componentId, statusModel);
     }
 
-    @Override
-    public void register(IResourceRegister resourceRegister) {
-        super.register(resourceRegister);
-        resourceRegister.addAutoComplete(WebsiteConfig.class, "widgets",
-                "        <widget-hello>\n" +
-                "          <id>hello-world</id>\n" +
-                "          <message>This is my first Onexus widget!</message>\n" +
-                "        </widget-hello>");
-    }
 }
