da158288c59d9995325e1c65cbe0cc5c730d2166
Jordi Deu-Pons <jordi@jordeu.net>
2013-10-17 15:50:12 +0200

    Authenticate git server using user tokens. Related issue #52

--- org.onexus.resource.manager/src/main/java/org/onexus/resource/manager/internal/ws/git/GitServlet.java
+++ org.onexus.resource.manager/src/main/java/org/onexus/resource/manager/internal/ws/git/GitServlet.java
@@ -18,20 +18,26 @@
 package org.onexus.resource.manager.internal.ws.git;
 
 
+import org.onexus.resource.api.IResourceManager;
 import org.onexus.resource.manager.internal.ProjectsContainer;
+import org.onexus.resource.manager.internal.ResourceManager;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 
 public class GitServlet extends org.eclipse.jgit.http.server.GitServlet implements ServletConfig {
 
     private ServletConfig config;
+    private IResourceManager resourceManager;
 
     @Override
     public void init(final ServletConfig config) throws ServletException {
         this.config = config;
+
+        setRepositoryResolver(new FileResolver<HttpServletRequest>((ResourceManager) resourceManager));
         super.init(this);
     }
 
@@ -58,5 +64,11 @@ public class GitServlet extends org.eclipse.jgit.http.server.GitServlet implemen
         return config.getInitParameterNames();
     }
 
+    public IResourceManager getResourceManager() {
+        return resourceManager;
+    }
 
+    public void setResourceManager(IResourceManager resourceManager) {
+        this.resourceManager = resourceManager;
+    }
 }
